ef597219941310a3336d61044a8ba26dba45b1aa
Mihai Razvan <MIHAIRAZVAN03@YAHOO.COM>
2023-04-27 20:22:58 +0300

    solved conflicts

--- app/src/main/java/teams/TeamsActivity.java
+++ app/src/main/java/teams/TeamsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +14,12 @@ import com.example.register.R;
 import authentication.FragmentLogIn;
 import database_connection.FileRequest;
 import interfaces.ActivityBasics;
+import profile.ProfileActivity;
 
 public class TeamsActivity extends AppCompatActivity implements ActivityBasics {
 
     private Button act_teams_fr_listing_menu_nav_team_button;
+    private Button act_teams_fr_listing_menu_nav_profile_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +35,13 @@ public class TeamsActivity extends AppCompatActivity implements ActivityBasics {
     @Override
     public void getActivityElements() {
         act_teams_fr_listing_menu_nav_team_button = findViewById(R.id.act_teams_fr_listing_menu_nav_team_button);
+        act_teams_fr_listing_menu_nav_profile_button = findViewById(R.id.act_teams_fr_listing_menu_nav_profile_button);
     }
 
     @Override
     public void setListeners() {
         act_teams_fr_listing_menu_nav_team_button_onClick();
+        act_teams_fr_listing_menu_nav_profile_button_onClick();
     }
 
     private void act_teams_fr_listing_menu_nav_team_button_onClick() {
@@ -44,6 +49,15 @@ public class TeamsActivity extends AppCompatActivity implements ActivityBasics {
             @Override
             public void onClick(View view) {
                 setTeamsListingFragment();
+            }
+        });
+    }
+
+    private void act_teams_fr_listing_menu_nav_profile_button_onClick() {
+        act_teams_fr_listing_menu_nav_profile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TeamsActivity.this, ProfileActivity.class));
             }
         });
     }
